1f23e0dde14311913b80ef2188c06f0a606849d7
SebasPCDev <sebpa.16@gmail.com>
2025-11-22 13:41:57 -0500

    rollback

--- build.sh
+++
@@ -1,8 +0,0 @@
-# Crear build.sh
-echo "#!/bin/bash
-composer install --optimize-autoloader --no-dev
-npm ci
-npm run build
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache" > build.sh
